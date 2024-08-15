133adf7542848d3856331566dd36c8f53c0bbab7
msrotte <profemairarotte@gmail.com>
2024-08-15 11:49:37 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/ejercicio2_g2/Ejercicio2_G2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio2_g2;
+
+/**
+ *
+ * @author enzoa
+ */
+public class Ejercicio2_G2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
