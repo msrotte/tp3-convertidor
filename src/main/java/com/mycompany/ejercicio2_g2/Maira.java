bc97c56665ce901e44a8e975749cd742aeb32f66
csaez30 <43256308+csaez30@users.noreply.github.com>
2024-08-15 11:58:57 -0300

    agregue clase maira
    ver cartel

---
+++ src/main/java/com/mycompany/ejercicio2_g2/Maira.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ejercicio2_g2;
+
+/**
+ *
+ * @author csaez
+ */
+public class Maira {
+    int edad=20;
+
+    public Maira() {
+        System.out.println("Maira tiene "+edad+" años");
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    
+    
+}
